aa532312d8295ee87ccc30200a7a81d610e6a1e5
ztenst <754710007@qq.com>
2018-10-16 11:01:49 +0800

    yh

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 let extConfig = {
     "env": "production",
     "app_id" : 1,
-    "host": "https://xwl.jj58.com.cn",
+    // "host": "http://zd.com",
+    "host": "https://gr.jjqapp.com",
+    // "host": "https://xwl.jj58.com.cn",
+    // "host": "https://xwl.jj58.com.cn",
     //"host": "https://xcx.shangxiaban.cn",
     "static_path": "http://hangjiayun.oss-cn-shanghai.aliyuncs.com/family/jc/images/"
 }
