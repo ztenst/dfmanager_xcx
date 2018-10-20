082b3a29884b91bee8c1898a86e7559224d9c086
ztenst <754710007@qq.com>
2018-10-20 13:51:29 +0800

    '随便'

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 let extConfig = {
     "env": "production",
     "app_id" : 1,
-    // "host": "http://zd.com",
-    "host": "https://gr.jjqapp.com",
+    "host": "http://zd.com",
+    // "host": "https://gr.jjqapp.com",
+    // "host": "https://meat.madridwine.cn",
     // "host": "https://xwl.jj58.com.cn",
-    // "host": "https://xwl.jj58.com.cn",
-    //"host": "https://xcx.shangxiaban.cn",
+    // "host": "https://xcx.shangxiaban.cn",
     "static_path": "http://hangjiayun.oss-cn-shanghai.aliyuncs.com/family/jc/images/"
 }
 extConfig = Object.assign(extConfig, wx.getExtConfigSync ? wx.getExtConfigSync() : {})
